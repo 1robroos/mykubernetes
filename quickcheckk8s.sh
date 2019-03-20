e518944fb72d97d9d7cd5582ab769e342095e6b8
Rob Roos <1robroos@gmail.com>
2019-03-20 19:00:46 +0100

    replace node name with variable

--- quickcheckk8s.sh
+++ quickcheckk8s.sh
@@ -2,8 +2,7 @@
 echo "Check node Condition"
 for node in ` kubectl get nodes| grep ip|awk '{ print $1}'`
 do
- #echo $node
- kubectl describe node/ip-10-1-2-206.us-west-2.compute.internal | grep -A6 Conditions| grep True
+ kubectl describe node/$node | grep -A6 Conditions| grep True
  RC=$?
  if [[ $RC == 0 ]]
  then
